54f03d98f3b56fd09682b9d26a969c9cffbbba3b
Stephan Fourie <stephanfourie0406@gmail.com>
2021-10-20 00:09:44 +0200

    I changed alot of stuff

---
+++ Integrated Project/Data Access/CRUD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Integrated_Project.Data_Access;
+using Integrated_Project.Models;
+
+namespace Integrated_Project.Data_Access
+{
+    public static class CRUD
+    {
+        public static class User
+        {
+            public static bool Status(string Email)
+            {
+                String QueryString = $"EXEC LoginCheck @Email = '{Email}';";
+                var output = Input_and_Output.Output(QueryString);
+
+                IDictionary<string, object> valuePairs = output[0];
+              //  bool x = (bool)valuePairs.FirstOrDefault();
+
+                if (true)
+                {
+                    return true;
+                }else
+                {
+                    return false;
+                }  
+            }
+
+            public static Models.User Get(string Email)
+            {
+                String QueryString = $"EXEC GetUserByEmail @Email = '{Email}';";
+                var output = Input_and_Output.Output(QueryString);
+
+                Models.User user = new Models.User();
+
+                
+                return user;
+            }
+
+        }
+
+    }
+}
---
+++ Integrated Project/Data Access/Input_and_Output.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Integrated_Project.Data_Access
+{
+    public static class Input_and_Output
+    {
+        public static void  Input(string QueryString)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("Sql_Database")))
+            {
+                throw new NotImplementedException();
+            }  
+        }
+
+        public static dynamic Output(string QueryString)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("Sql_Database")))
+            {
+                dynamic output = connection.Query<dynamic>(QueryString).ToList();
+                return output;
+            }
+        }
+    }
+}
